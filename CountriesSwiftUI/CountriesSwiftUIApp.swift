63d550bced8859f7a92282dc32a7aa4dfeb2e3e1
ahamula <andriihamula@gmail.com>
2024-02-14 12:32:43 +0200

    Initial Commit

---
+++ CountriesSwiftUI/CountriesSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesSwiftUIApp.swift
+//  CountriesSwiftUI
+//
+//  Created by Andrii Hamula on 14.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct CountriesSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
